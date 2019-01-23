e4dc587472d6edd54a7ae16774e41e4e9b243242
Brandon <Blent1050@yahoo.com>
2019-01-23 17:38:58 -0500

    added ui for adding a friend

--- friends/src/components/AddFriend.jsx
+++ friends/src/components/AddFriend.jsx
@@ -1,8 +1,62 @@
 import React from 'react';
-
+import styled from 'styled-components';
+import FormGroup from '@material-ui/core/FormGroup';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
 
 const AddFriend = (props) => {
-    return <h1>Add Friend</h1>
-}
+	return (
+		<AddFriendContainer>
+			<h1>Add a friend!</h1>
+			<FormGroup>
+				<Inputs />
+			</FormGroup>
+		</AddFriendContainer>
+	);
+};
 
 export default AddFriend;
+
+const AddFriendContainer = styled.div`
+	margin: 0 auto;
+	width: 50%;
+    text-align: center;
+    form{ 
+        margin-bottom: 10px;
+    }
+`;
+
+function Inputs(props) {
+	const { classes } = props;
+	return (
+		<div>
+			<Input
+				placeholder="Name"
+				inputProps={{
+					'aria-label': 'Description'
+				}}
+				type="text"
+			/>{' '}
+			<br />
+			<Input
+				placeholder="Age"
+				inputProps={{
+					'aria-label': 'Description'
+				}}
+				type="number"
+			/>
+			<br />
+			<Input
+				placeholder="Email"
+				inputProps={{
+					'aria-label': 'Description'
+				}}
+				type="email"
+			/>
+			<br /> <br />
+			<Button variant="contained" color="primary">
+				Submit
+			</Button>
+		</div>
+	);
+}
