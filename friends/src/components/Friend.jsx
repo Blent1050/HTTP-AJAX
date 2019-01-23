379b226d63f2cb7aefacd3f8fe80ae3f85793c0b
Brandon <Blent1050@yahoo.com>
2019-01-23 15:57:38 -0500

    added styled-components styling to friend component

--- friends/src/components/Friend.jsx
+++ friends/src/components/Friend.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+
+
 
 const Friend = (props) => {
 	return (
-		<div>
-			<h2>{props.friend.name}</h2>
+		<FriendContainer>
+			<FriendName>{props.friend.name}</FriendName>
 			<p>Age: {props.friend.age}</p>
 			<p>Email: {props.friend.email}</p>
-		</div>
+		</FriendContainer>
 	);
 };
 
 export default Friend;
+
+
+const FriendContainer = styled.div`
+	text-align: center;
+	box-shadow: -1px 1px 31px -5px rgba(0,0,0,0.16);
+	border: 1px solid #e6e6e6;
+	width: 50%;
+	margin: 5px auto;
+	padding: 5px;
+`
+const FriendName = styled.h2`
+	margin: 0;
+	padding: 0;
+`
