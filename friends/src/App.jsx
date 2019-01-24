e245e5cc5147d40197c2eeee1f0cb7e1637a3332
Brandon <Blent1050@yahoo.com>
2019-01-24 12:11:48 -0500

    poating to database created

--- friends/src/App.jsx
+++ friends/src/App.jsx
@@ -8,6 +8,9 @@ import axios from 'axios';
 class App extends Component {
 	state = {
 		friends: [],
+		name: '',
+		age: 0,
+		email: '',
 		error: ''
 	};
 
@@ -18,15 +21,40 @@ class App extends Component {
 			.catch((err) => this.setState({ error: 'Error' }));
 	}
 
+	postMessage = (e) => {
+		e.preventDefault();
+        axios
+          .post(`http://localhost:5000/friends`, { 
+			  name:this.state.name,
+			  age:this.state.age,
+			  email:this.state.email
+		  })
+          .then(res =>{
+			this.setState({
+				friends: res.data,
+				name: '',
+				age: '',
+				email:''
+			})
+		  })
+          .catch(err => console.log(err));
+	};
+
+	handleChanges = event => {
+		this.setState({ 
+			[event.target.name]: event.target.value });
+	  };
+
 	render() {
+		console.log(this.state.friends)
 		return (
 			<div className="App">
 				<nav className='nav'>
-					<NavLink to="/">Home</NavLink>
+					<NavLink exact to="/">Home</NavLink>
 					<NavLink to="/add">Add Friends</NavLink>
 				</nav>
 				<Route exact path='/' render={props => <FriendContainer {...props} friends={this.state.friends}/>}/>
-				<Route path='/add' component={AddFriend}/>
+				<Route path='/add' render={ props => <AddFriend {...props} friend={this.state} handleChanges={this.handleChanges} postMessage={this.postMessage} />}/>
 			</div>
 		);
 	}
--- friends/src/components/AddFriend.jsx
+++ friends/src/components/AddFriend.jsx
@@ -4,16 +4,20 @@ import FormGroup from '@material-ui/core/FormGroup';
 import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
 
-const AddFriend = (props) => {
+const AddFriend = props => {
+	const {friends, friend, error} = props;
 	return (
 		<AddFriendContainer>
 			<h1>Add a friend!</h1>
 			<FormGroup>
-				<Inputs />
+			<form onSubmit={props.postMessage}>
+				<Inputs friend={friend} handleChanges={props.handleChanges}/>
+			</form>
 			</FormGroup>
 		</AddFriendContainer>
 	);
-};
+}
+
 
 export default AddFriend;
 
@@ -27,19 +31,25 @@ const AddFriendContainer = styled.div`
 `;
 
 function Inputs(props) {
-	const { classes } = props;
+	console.log(props)
 	return (
-		<div>
+		<>
 			<Input
 				placeholder="Name"
+				value={props.friend.name}
+				onChange={props.handleChanges}
+				name='name'
 				inputProps={{
 					'aria-label': 'Description'
 				}}
-				type="text"
+				
 			/>{' '}
 			<br />
 			<Input
 				placeholder="Age"
+				value={props.friend.age}
+				name='age'
+				onChange={props.handleChanges}
 				inputProps={{
 					'aria-label': 'Description'
 				}}
@@ -47,16 +57,19 @@ function Inputs(props) {
 			/>
 			<br />
 			<Input
+				value={props.friend.email}
 				placeholder="Email"
+				name='email'
+				onChange={props.handleChanges}
 				inputProps={{
 					'aria-label': 'Description'
 				}}
 				type="email"
 			/>
 			<br /> <br />
-			<Button variant="contained" color="primary">
+			<Button type='submit' variant="contained" color="primary">
 				Submit
 			</Button>
-		</div>
+		</>
 	);
 }
